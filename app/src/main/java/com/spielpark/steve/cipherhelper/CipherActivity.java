6c73f45cf2202252234440ac860993b5e6d397da
shadowwolf0189 <steve.vernyi@gmail.com>
2015-03-30 16:52:00 -0400

    Transposition 50% done.

--- app/src/main/java/com/spielpark/steve/cipherhelper/CipherActivity.java
+++ app/src/main/java/com/spielpark/steve/cipherhelper/CipherActivity.java
@@ -2,16 +2,11 @@ package com.spielpark.steve.cipherhelper;
 
 import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+
 
 
 public class CipherActivity extends FragmentActivity {
